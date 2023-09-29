a2750f8a1f41dc21254e775bd43b9273ef1aee82
Jiofef <jiofef@gmail.com>
2023-09-30 02:39:54 +0300

    level 10 background added

---
+++ Content/Scripts/Other/AnimationRandomizer.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class AnimationRandomizer : AnimationPlayer
+{
+	string[] _animationsArray;
+	public override void _Ready()
+	{
+		_animationsArray = GetAnimationList();
+		Connect("animation_finished", new Callable(this, "ChangeAnimation"));
+	}
+
+	public void ChangeAnimation(string junk)
+	{
+		Random random = new Random();
+		CurrentAnimation = _animationsArray[random.Next(_animationsArray.Length)];
+	}}
