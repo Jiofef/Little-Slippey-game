cf4cbd431c4f87ecc3a2f361c75db25c4dc832a4
Jiofef <jiofef@gmail.com>
2023-10-30 02:11:03 +0300

    Minor skin changes

--- Content/Scripts/Other/DefaultSkinAPIXD.cs
+++ Content/Scripts/Other/DefaultSkinAPIXD.cs
@@ -4,11 +4,19 @@ using System;
 public partial class DefaultSkinAPIXD : AnimatedSprite2D
 {
 	[Signal] public delegate void PlayerDeathEventHandler();
+	[Export] bool _enableSecondJumpAndFallFrame = false;
 	public override void _Ready()
 	{
 		GetParent().Connect("PlayerDied", new Callable(this, "PlayerDied"));
+		SetPhysicsProcess(_enableSecondJumpAndFallFrame);
 	}
-	public void PlayerDied()
+    public override void _PhysicsProcess(double delta)
+    {
+		if (_enableSecondJumpAndFallFrame && (Animation == "Fall" || Animation == "Jump"))
+			Frame = Input.IsActionPressed("ui_right") || Input.IsActionPressed("ui_left") ? 1 : 0;
+			
+    }
+    public void PlayerDied()
 	{
 		EmitSignal("PlayerDeath");
     }
